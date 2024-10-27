1e6f0daa9172a372b03487d08a242c93f99ae735
Kopeechka <chernykhnik21@gmail.com>
2024-10-27 14:40:51 +0300

    Лабораторная работа №2
    Лабораторная работа №2! Теперь и она! Прямо в день мягкого дедлайна

---
+++ Лабораторная 2/task4.py
@@ -0,0 +1,14 @@
+money_capital = 20000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+#как с count, начинаю отсчёт с нуля для цикла
+months = 0
+#цикл будет повторяться, пока расходы не будут больше суммы подушки и зарплаты
+while money_capital + salary >= spend:
+    money_capital += salary - spend
+    spend *= 1 + increase
+    months += 1
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
