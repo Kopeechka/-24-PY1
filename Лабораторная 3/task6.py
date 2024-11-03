89607aeaf876c5c2cde4bf4f802c69cfe0b6a2fb
Kopeechka <chernykhnik21@gmail.com>
2024-11-03 22:20:30 +0300

    Лабораторная №3
    Лабораторная №3 - это действительно лабораторная №3 по теме "функции и методы"

---
+++ Лабораторная 3/task6.py
@@ -0,0 +1,15 @@
+def search_index(items_list, goal_item):
+    if goal_item in items_list:
+        return items_list.index(goal_item)
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
